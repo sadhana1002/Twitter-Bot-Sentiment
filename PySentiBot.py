b34ac7dbd034ad29a04ec631a51963a82f4e3da6
unknown <sadhanarani.sankar@gmail.com>
2017-11-26 13:25:13 -0600

    Requirements for Heroku

---
+++ PySentiBot.py
@@ -0,0 +1,167 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+# Dependencies
+import tweepy
+import json
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+import os
+
+import time
+
+plt.style.use('seaborn')
+
+
+# In[ ]:
+
+
+# Import and Initialize Sentiment Analyzer
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+analyzer = SentimentIntensityAnalyzer()
+
+
+consumer_key = os.getenv("bot_consumer_key")
+consumer_secret = os.getenv("bot_consumer_secret")
+access_token = os.getenv("bot_access_token")
+access_token_secret = os.getenv("bot_access_token_secret")
+
+
+# In[ ]:
+
+
+# Setup Tweepy API Authentication
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
+
+
+# In[ ]:
+
+
+def parse_requests(tweet, tweet_dict=dict()):
+    tweet_data = []
+    tweet_id = tweet["id"]
+    tweet_user = tweet["user"]["screen_name"]
+    tweet_requests = []
+    print(tweet_id)
+    for mentions in tweet["entities"]["user_mentions"]:
+        if mentions["screen_name"] != "PySentiBot":
+            tweet_requests.append(mentions["screen_name"])
+    
+    tweet_dict = {"id":tweet_id,"user":tweet_user,"analysis_requests":tweet_requests}
+    return tweet_dict
+
+
+# In[ ]:
+
+
+def analyze_sentiments(recent_tweets, sentiment_results=list()):
+    sentiment_results = []
+    for tweet in recent_tweets:
+        new_tweet = cleanse_tweet(tweet)
+        sentiment_result = analyzer.polarity_scores(new_tweet["text"])
+        sentiment_results.append(sentiment_result)    
+    return sentiment_results
+
+
+# In[ ]:
+
+
+def remove_noise(tweet, category, key, result_tweet=dict()):
+    try:
+        result_tweet = tweet
+        tweet_text = tweet.get("text")
+        tweet_items = tweet.get("entities").get(category)
+        for item in tweet_items:
+            replace_str = item[key]
+            tweet_text = tweet_text.replace(replace_str," ")
+        result_tweet["text"] = tweet_text
+    except TypeError:
+        pass
+    return result_tweet
+
+def cleanse_tweet(tweet,result_tweet=dict()):
+    result_tweet = tweet
+    result_tweet = remove_noise(result_tweet,"user_mentions","screen_name")
+    result_tweet = remove_noise(result_tweet,"urls","url")
+    result_tweet = remove_noise(result_tweet,"media","url")
+    result_tweet["text"] = result_tweet["text"].replace("@","")
+    return result_tweet
+
+
+# In[ ]:
+
+
+def color_map(value):
+    if(value >= 0): 
+        return 'g'
+    else:
+        return 'r'
+
+def plot_sentiments(title,sentiments):
+    df = pd.DataFrame(sentiments)
+    df = df.reset_index()
+#     df.plot(kind="scatter",x="index",y="compound",marker="o")
+    df.plot( 'index', 'compound', linestyle='-', marker='o',alpha=0.75)
+    plt.ylabel("Sentiment score")
+    plt.xlabel("Tweets")
+    plt.title(title)
+    
+    filename = "SentimentAnalysis_of_"+title+".png"
+    plt.savefig(filename)
+    
+    return filename  
+
+
+# In[ ]:
+
+
+def scan_for_requests(since_tweet_id):
+    search_handle = "@PySentiBot"
+
+    results = api.search(search_handle,since_id = since_tweet_id)
+
+    if(len(results["statuses"]) > 0):
+        tweet_data = []
+
+        for tweet in results["statuses"]:
+            tweet_data.append(parse_requests(tweet))
+        
+        for item in tweet_data:
+
+            recent_tweets = []
+
+            for analyze_request in item["analysis_requests"]:
+
+                recent_tweets = api.user_timeline(analyze_request,count=200)
+
+                if(len(recent_tweets) > 0):
+                    sentiments = analyze_sentiments(recent_tweets)
+                    print(sentiments)
+                    sentiment_fig = plot_sentiments(analyze_request,sentiments)
+                    text_status = f"TRY:11_23:11_30 Thank you for your tweet @{item['user']}! Here is the sentiment analysis of {analyze_request}!"
+                    api.update_with_media(filename=sentiment_fig,status=text_status,in_reply_to_status_id=item["id"])
+                else:
+                    text_status = f"TRY:11_23:11_30 Thank you for your tweet @{item['user']}! Sorry, {analyze_request} has no tweets!" 
+                    api.update_status(text_status)
+
+                plt.show()
+        return results["statuses"][0]["id"]
+    else:
+        return since_tweet_id
+
+
+# In[ ]:
+
+
+since_tweet_id = 933930057860599809
+while True:
+    since_tweet_id = scan_for_requests(since_tweet_id)
+    time.sleep(300)
+
